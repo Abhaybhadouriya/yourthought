f11b2ddd4adf7f85208ab5a28ec2e4ae42c69651
Abhay Bhadouriya <sbhadouriya39@gmail.com>
2022-12-25 17:42:47 +0530

    v1

--- client/controller/document.js
+++ client/controller/document.js
@@ -6,13 +6,21 @@ const { v4: uuidv4 } = require("uuid");
 
 module.exports.updateDocument = async (req, res) => {
   try {
-    const { title, content, userId,tags } = req.body;
+    const { title, content, userId, tags, name, docId } = req.body;
+    const fs = firebase.firestore();
 
-
-    return res.status(200).json({
-      status: 200,
-      message: "Updated successfully",
+    fs.collection('documents').doc(docId).set({
+      title: title,
+      name: name,
+      userId: userId,
+      tag: tags,
+      content: content
+    })
+    return res.status(201).json({
+      message: "Blog is Updated Successfully",
     });
+
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -24,22 +32,30 @@ module.exports.updateDocument = async (req, res) => {
 
 module.exports.saveDocument = async (req, res) => {
   try {
-    const { title, content, userId,tags } = req.body;
+    const { title, content, userId, tags, name, date } = req.body;
     const docId = uuidv4()
     const fs = firebase.firestore();
 
     fs.collection('documents').doc(docId).set({
-      title:title,
-      name:name,
-      date:date,
-      tag:tag,
-      content:content
+      title: title,
+      name: name,
+      userId: userId,
+      tag: tags,
+      content: content
     })
+    const documentData = {
+      id: docId,
+      userId: userId,
+      title: title,
+      tags: tags
+    }
 
-    return res.status(200).json({
-      status: 200,
-      message: "Saved successfully",
+    models.Document.create(documentData)
+    return res.status(201).json({
+      message: "Blog is Successfully saved",
     });
+
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -51,7 +67,25 @@ module.exports.saveDocument = async (req, res) => {
 
 module.exports.deleteDocument = async (req, res) => {
   try {
-    
+    const { docId } = req.body;
+    firebase.firestore().collection('documents').doc(docId).delete()
+    const document = await models.Document.findOne({
+      where: {
+        id: docId
+      },
+    });
+
+    if (!document) {
+      return res.status(404).json({
+        message: "Document not found",
+      });
+    }
+
+    await models.Document.destroy({
+      where: {
+        id: docId
+      },
+    });
     return res.status(200).json({
       status: 200,
       message: "Delete successfully",
@@ -66,80 +100,121 @@ module.exports.deleteDocument = async (req, res) => {
 };
 
 module.exports.viewDocument = async (req, res) => {
-    try {
-      
+  try {
+    const { docId } = req.body;
+    const docRef = firebase.firestore().collection('documents').doc(docId)
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+      return res.status(404).json({
+        status: 404,
+        message: "No such document!",
+      });
+    } else {
+
       return res.status(200).json({
         status: 200,
+        doc: doc.data(),
         message: "Document Fetched Successfully",
       });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        status: 500,
-        message: "Internal server error occured. Please try again",
-      });
     }
-  };
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error occured. Please try again",
+    });
+  }
+};
 
-  module.exports.viewDocumentList = async (req, res) => {
-    try {
-      
-      return res.status(200).json({
-        status: 200,
-        message: "Document List Fetched successfully",
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        status: 500,
-        message: "Internal server error occured. Please try again",
-      });
-    }
-  };
-  module.exports.filterList = async (req, res) => {
-    try {
-      
-      return res.status(200).json({
-        status: 200,
-        message: "Filtered List Fetched successfully",
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        status: 500,
-        message: "Internal server error occured. Please try again",
-      });
-    }
-  };
+module.exports.viewDocumentListUser = async (req, res) => {
+  try {
 
-  module.exports.publishDocument = async (req, res) => {
-    try {
-      
-      return res.status(200).json({
-        status: 200,
-        message: "Document Published successfully",
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        status: 500,
-        message: "Internal server error occured. Please try again",
+    const { userId } = req.body;
+    const docRef = firebase.firestore().collection('documents')
+    const snapshot = await docRef.where('userId', '==', userId).get();
+    if (snapshot.empty) {
+      return res.status(404).json({
+        status: 404,
+        message: "No such document!",
       });
     }
-  };
 
-  module.exports.unpublishDocument = async (req, res) => {
-    try {
-      
-      return res.status(200).json({
-        status: 200,
-        message: "Document Published successfully",
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        status: 500,
-        message: "Internal server error occured. Please try again",
-      });
-    }
-  };
+    // snapshot.forEach(doc => {
+    //   console.log(doc.id, '=>', doc.data());
+    // });
+
+    return res.status(200).json({
+      data: snapshot,
+      status: 200,
+      message: "Document List Fetched successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error occured. Please try again",
+    });
+  }
+};
+module.exports.filterList = async (req, res) => {
+  try {
+    const { query } = req.body
+    documents = await models.Document.findAll({
+      where: {
+        published:1,
+        [Op.or]: [{ title: { [Op.like]: `%${query}%` } },
+        { tags: { [Op.like]: `%${query}%` } }]
+      },
+    });
+
+    return res.status(200).json({
+      document: documents,
+      status: 200,
+      message: "Filtered List Fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error occured. Please try again",
+    });
+  }
+};
+
+module.exports.publishDocument = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    await models.Document.update({published:1},{
+      where:{id:docId}
+    })
+    return res.status(200).json({
+      status: 200,
+      message: "Document Published successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error occured. Please try again",
+    });
+  }
+};
+
+module.exports.unpublishDocument = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    await models.Document.update({published:0},{
+      where:{id:docId}
+    })
+    return res.status(200).json({
+      status: 200,
+      message: "Document Published successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error occured. Please try again",
+    });
+  }
+};
--- client/routes/document.js
+++ client/routes/document.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/document");
 
-router.get("/saveDocument", (req, res) => {
+router.post("/saveDocument", (req, res) => {
     controller.saveDocument(req, res);
 });
   router.get("/deleteDocument", (req, res) => {
     controller.deleteDocument(req, res);
   });
-  router.get("/updateDocument", (req, res) => {
+  router.post("/updateDocument", (req, res) => {
     controller.updateDocument(req, res);
   });
   router.get("/viewDocument", (req, res) => {
     controller.viewDocument(req, res);
   });
-  router.get("/viewDocumentList", (req, res) => {
-    controller.viewDocumentList(req, res);
+  router.get("/viewDocumentListUser", (req, res) => {
+    controller.viewDocumentListUser(req, res);
   });
   router.get("/filterList", (req, res) => {
     controller.filterList(req, res);
