eaf943d87341c04ceef318bfcd7afff12f616416
Abhay Bhadouriya <sbhadouriya39@gmail.com>
2022-12-28 10:12:29 +0530

    v1

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.set("trust proxy", true);
 
 app.use(
   cors({
-    origin: [allConfig.client_url],
+    origin: '*',
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
     credentials: true,
   })
@@ -64,13 +64,13 @@ app.get("/", (req, res) => {
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://youthought.abhaybhadouriya.tech/');
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+// 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Origin, Content-Type, Accept');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
